5297e32fd5ab9e6d36517ad65bba86cf58a3f49c
fajla rabbi <fajlarabbi1991@gmail.com>
2021-08-09 08:56:52 +0600

    change config and change pom file

---
+++ spring-mvc-with-hibernet/src/main/java/com/fajle/config/HibernateConfig.java
@@ -0,0 +1,65 @@
+package com.fajle.config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+
+import javax.persistence.Entity;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Modifier;
+import java.util.Properties;
+
+//@org.springframework.context.annotation.Configuration
+public class HibernateConfig {
+    private SessionFactory sessionFactory = null;
+
+    private Session session;
+
+    //    @Bean
+    public Session getSession() {
+        this.session = createAndGetLocalSessionFactoryBean().getCurrentSession();
+        return session != null
+                ? this.session
+                : createAndGetLocalSessionFactoryBean().openSession();
+    }
+
+    //    @Bean
+    public SessionFactory createAndGetLocalSessionFactoryBean() {
+        if (this.sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = getBuiltProperties("hibernate.properties");
+
+                configuration.setProperties(settings);
+                configuration.addPackage("com.spring5.practice.model");
+                for (Class<?> clazz : (new Reflections("com.spring5.practice.model")).getTypesAnnotatedWith(Entity.class)) {
+                    if (!Modifier.isAbstract(clazz.getModifiers())) {
+                        configuration.addAnnotatedClass(clazz);
+                    }
+                }
+                StandardServiceRegistryBuilder serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(settings);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry.build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    private Properties getBuiltProperties(String propertyFileName) {
+        Properties properties = new Properties();
+        InputStream input = HibernateConfig.class
+                .getClassLoader().getResourceAsStream(propertyFileName);
+        try {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
